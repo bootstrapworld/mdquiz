137b9eb0baf734ea1c0e92565f3f6a7da555e56b
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2025-04-04 16:10:49 -0500

    vite-plugin-singlefile inlines CSS and JS, but not image files. add the static-copy plugin, and instruct it to copy all image files

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { viteSingleFile } from "vite-plugin-singlefile"
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,5 +13,14 @@ export default defineConfig({
   plugins: [
     react(), 
     viteSingleFile(),
+    // copy all image files, since they are not inlined
+    viteStaticCopy({
+      targets: [
+        {
+          src: ['*.svg', '*.png', '*.gif', '*.jpg'],
+          dest: '../dist'
+        }
+      ]
+    })
   ],  
 })
