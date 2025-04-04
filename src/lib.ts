ea55b1e05d944c5cf362fe2ea7ddb6b0e762ecc0
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2025-04-04 17:48:55 -0500

    refactor some imports into lib.ts

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,16 @@
+import { parse } from 'smol-toml'
+import { type Quiz } from "./bindings/Quiz";
+import "./telemetry";
+
+declare global {
+  interface Window {
+      telemetry:any;
+  }
+}
+
 export * from "./questions/mod";
 export * from "./components/quiz";
 export * from "./components/snippet";
 export { type Quiz } from "./bindings/Quiz";
+export const parseTOML = (tomlStr) => (parse(tomlStr) as any as Quiz)
+
--- src/quiz.tsx
+++ src/quiz.tsx
@@ -1,19 +1,11 @@
-import { parse } from 'smol-toml'
-import { type Quiz, QuizView } from "./lib";
+import { parseTOML, type Quiz, QuizView } from "./lib";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import quizStr from "./quiz.toml?raw";
-import "./telemetry";
-
-declare global {
-  interface Window {
-      telemetry:any;
-  }
-}
 
 const App = () => {
-  const quiz = parse(quizStr) as any as Quiz;
+  const quiz = parseTOML(quizStr) as Quiz;
   return (
     <div>
       <h1>Example quiz</h1>
