c74ee9a6a81402a4791d80200ff0fdac72b1f3fa
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2025-05-13 10:15:10 -0400

    add loading indicator

--- src/components/pyret-snippet.tsx
+++ src/components/pyret-snippet.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { makeEmbed } from "pyret-embed";
+import { RotatingLines } from "react-loader-spinner";
 
 export interface SnippetOptions {
   program: string;
@@ -8,10 +9,18 @@ export interface SnippetOptions {
 }
 
 export const PyretSnippet: React.FC<SnippetOptions> = options => {
+  // state for showing loading indicator
+  const [isLoading, setIsLoading] = useState(true);
+
+  // create a ref, so that we can render into the DOM
   const ref = useRef<HTMLDivElement>(null);
   useEffect( () => {
     const setupEditor = async () => {
       const editor = await makeEmbed("EmbeddedEditor", ref.current, "https://pyret-horizon.herokuapp.com/editor")
+
+      // hide the loading indicator
+      setIsLoading(false);
+
       editor.sendReset({
         definitionsAtLastRun: options.program,
         interactionsSinceLastRun: ["r"],
@@ -21,5 +30,23 @@ export const PyretSnippet: React.FC<SnippetOptions> = options => {
     }
     setupEditor();
   }, [options]);
-  return <div ref={ref} className="embedded-editor"/>;
+
+  // the "loading" spinner, which is null if we're done loading
+  const spinner = isLoading? (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="96"
+          visible={true}
+        />
+      </div>
+    ) : null;
+
+  return (
+    <div>
+      {spinner}
+      <div ref={ref} className="embedded-editor"/>;
+    </div>)
 };
